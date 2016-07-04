b280a54e050f6f4e7f7255e062c0f4658dc53a78
noemiko <noemikow8@gmail.com>
2016-07-04 20:42:50 +0200

    move promise and create structure logic to service

--- js/menu/menuController.js
+++ js/menu/menuController.js
@@ -1,48 +1,19 @@
 angular.module('Menu')
-.controller('menuController', ['$scope','$http',function ($scope,$http) {
+.controller('menuController', ['$scope','$http','$routeParams', 'groupList', 'structureMenu' ,
+function ($scope,$http, $routeParams, groupList, structureMenu) {
 	var menu=this;
-		$http({
-				  method: 'GET',
-				  url: '../php/API/createMenu.php'
-				})
-  		 .success(function(data, status, headers, config) 
-			    {
-			    menu.structure=getStrutureMenuFromJson(data);
-			    console.table(menu.structure);
-				})
-  		 .error(function(data, status, headers, config)
-  		 		 {
-			      alert('Z powodu błędu nie można było załadować piktogramów');
-				});
-  		
-  		getStrutureMenuFromJson=function(data)
-  		{
-  			menuList={};
-  			for (i = 0; i < data.length; i++)
-  			{		console.log(data);
-  					var item = data[i];
-  					var fieldOfStudyWithMode = item[3]+"("+item[4]+")";
-  					var group = item[1];
-  					var subjectWithType = item[0]+"("+item[2]+")";
 
-  				if (typeof menuList[fieldOfStudyWithMode] != 'undefined')
-  				{
-  					if (typeof menuList[fieldOfStudyWithMode][group] == 'undefined')
-  					{
-  						menuList[fieldOfStudyWithMode][group]={subjects: []};
-  					}
-  					menuList[fieldOfStudyWithMode][group]['subjects'].push(subjectWithType);	
-  				}
-  				else
-  				{	
-  					menuList[fieldOfStudyWithMode]={};
-  					menuList[fieldOfStudyWithMode][group]={subjects: [subjectWithType]};
-  				}
-  					
-  			};
-  			console.log(menuList);
-  			return menuList;	
-  		};
+  groupList.getData().then(function(d){
+    menu.structure=structureMenu.setStructure(d);
+  });
+     
+
+
+  		
+
+
+
+ 
 
 
 }]);
--- js/menu/services.js
+++ js/menu/services.js
@@ -1,8 +1,47 @@
-angular.module('myApp',[]).
-service('groupList',['$http', function($http) {
+angular.module('myApp')
+  .factory('groupList', function ($http) {
+  return {
+        getData: function() {
+            return $http({
+				  method: 'GET',
+                  url: 'php/API/createMenu.php'
+				})
+  		 .error(function(data, status, headers, config)
+  		 		 {
+			      alert('Z powodu błędu nie można było załadować piktogramów');
+				});
 
-        return $http({
-                  method: 'GET',
-                  url: '../php/API/createMenu.php'
-                     });
-}]);
+  							}
+
+}});
+
+angular.module('myApp')
+  .service('structureMenu', function ($http) {
+  	 this.setStructure=function(data)
+  		{
+  			menuList={};
+  			data=data.data
+  			for (i = 0; i < data.length; i++)
+  			{	
+  					var item = data[i];
+  					var fieldOfStudyWithMode = item[3]+"("+item[4]+")";
+  					var group = item[1];
+  					var subjectWithType = item[0]+"("+item[2]+")";
+
+  				if (typeof menuList[fieldOfStudyWithMode] != 'undefined')
+  				{
+    					if (typeof menuList[fieldOfStudyWithMode][group] == 'undefined')
+    					{
+    						menuList[fieldOfStudyWithMode][group]={subjects: []};
+    					}
+  					menuList[fieldOfStudyWithMode][group]['subjects'].push(subjectWithType);	
+  				}
+  				else
+  				{	
+  					menuList[fieldOfStudyWithMode]={};
+  					menuList[fieldOfStudyWithMode][group]={subjects: [subjectWithType]};
+  				}	
+  			};
+  			return menuList;	
+  		};
+  	});
