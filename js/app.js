049cbcbf3be0850c793946d51af1cd7ed836a767
noemiko <noemikow8@gmail.com>
2016-07-04 20:43:30 +0200

    add ngRoute

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-angular.module('myApp', ['Menu']);
+angular.module('myApp', ['Menu','ngRoute'])
+	.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/start.html',
+     
+      })
+      .when('/groups/:fieldOfStudy/:group/:subject', {
+        templateUrl: 'partials/contentGroups.html',
+       controller:'menuController',
+       controllerAs:'menu'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+  
+  });
+
+
 
 angular.module('Menu', []);
